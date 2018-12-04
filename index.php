98a6fd1304b921eb494227c2cae8b74d87e40d8a
Gilson Sales <gilson.sales21@hotmail.com>
2018-12-04 16:43:11 -0300

    html básico
    base simples em html para divisão em 3 ares depois.

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Tema WP</title>
+</head>
+<body <?php body_class(); ?>>
+	<header>
+		<section class="top-bar">
+			<div class="social-media-icons"></div>
+			<div class="search"></div>
+		</section>
+	</header>
+	<div class="coment-area">
+		<main>
+			<section class="slide"></section>
+			<section class="services"></section>
+			<section class="middle-area">
+				<aside class="sidebar"></aside>
+				<div class="news"></div>
+			</section>
+			<section class="map"></section>
+		</main>
+	</div>
+
+	<footer>
+		
+	</footer>
+
+</body>
+</html>
